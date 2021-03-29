bdbc2a4d814408645c3660f84bfec7be482ee625
Matt <matt.furlong.12@gmail.com>
2021-03-29 11:40:27 -0400

    removed unnecessary elements. fixed styling to match site. added amount of days by month. added image for background image on selected item

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const date_picker_element = document.querySelector('.date-picker');
+// const date_picker_element = document.querySelector('.date-picker');
 const selected_date_element = document.querySelector('.date-picker .selected-date');
 const dates_elements = document.querySelector('.date-picker .dates');
 const month_element = document.querySelector('.date-picker .dates .month .mth');
@@ -26,17 +26,17 @@ selected_date_element.dataset.value = selectedDate;
 populateDate();
 
 // EVENT LISTENERS
-date_picker_element.addEventListener('click', toggleDatePicker);
+// date_picker_element.addEventListener('click', toggleDatePicker);
 next_month_element.addEventListener('click', goToNextMonth);
 prev_month_element.addEventListener('click', goToPrevMonth);
 
 // FUNCTIONS
-function toggleDatePicker(e){
-    console.log(e.path);
-    if (!checkEventPathForClass(e.path, 'dates')){
-        dates_elements.classList.toggle('active');
-    }
-}
+// function toggleDatePicker(e){
+//     console.log(e.path);
+//     if (!checkEventPathForClass(e.path, 'dates')){
+//         dates_elements.classList.toggle('active');
+//     }
+// }
 
 function goToNextMonth(e){
     month++;
@@ -50,7 +50,7 @@ function goToNextMonth(e){
 
 function goToPrevMonth(e){
     month--;
-    if(month > 0){
+    if(month < 0){
         month = 11;
         year--;
     }
@@ -60,11 +60,34 @@ function goToPrevMonth(e){
 
 function populateDate(e){
     days_element.innerHTML = "";
-    // make sure to make it dynamic based on what month is chosen
-    // if(month == 1){
-    //     amountOfDays = 28;
-    // } etc.
     let amountOfDays = 31;
+
+    //assign correct number of days depending on month
+    if(month == 0){
+        amountOfDays = 31;
+    }else if(month == 1){
+        amountOfDays = 28;
+    }else if(month == 2){
+        amountOfDays = 31;
+    }else if(month == 3){
+        amountOfDays = 30;
+    }else if(month == 4){
+        amountOfDays = 31;
+    }else if(month == 5){
+        amountOfDays = 30;
+    }else if(month == 6){
+        amountOfDays = 31;
+    }else if(month == 7){
+        amountOfDays = 31;
+    }else if(month == 8){
+        amountOfDays = 30;
+    }else if(month == 9){
+        amountOfDays = 31;
+    }else if(month == 10){
+        amountOfDays = 30;
+    }else if(month == 11){
+        amountOfDays = 31;
+    };
 
     for (let i = 0; i < amountOfDays; i++){
         const day_element = document.createElement('div');
